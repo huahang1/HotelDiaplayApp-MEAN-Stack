371cb70779d029ecd7818fb0aa8ac7af1074ac2c
Chris Hua <hanghua@HANGs-MacBook-Air.local>
2017-04-07 13:56:47 -0500

    finish the front-end part to make this web single page application

---
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -0,0 +1,40 @@
+angular.module('meanhotel').controller('HotelController',HotelController);
+
+function HotelController($route,$routeParams,hotelDataFactory) {
+
+    var vm = this;
+    var id = $routeParams.id;
+    vm.isSubmitted = false;
+
+    //get specific hotel info
+    hotelDataFactory.hotelDisplay(id).then(function (response) {
+        vm.hotel = response.data;
+        vm.stars = getStarRating(response.data.stars);
+    });
+
+    function getStarRating(stars) {
+        return new Array(stars);
+    }
+
+    vm.addReview = function () {
+
+        var postData = {
+            name:vm.name,
+            rating:vm.rating,
+            review:vm.review
+        };
+
+        if (vm.reviewForm.$valid){
+            hotelDataFactory.postReview(id, postData).then(function (response) {
+                if (response.status === 200){
+                    $route.reload();
+                }
+            }).catch(function (error) {
+                console.log(error);
+            })
+        }else{
+            vm.isSubmitted = true;
+        }
+    };
+
+}
---
+++ public/angular-app/hotel-rating/hotel-rating-directive.js
@@ -0,0 +1,8 @@
+angular.module('meanhotel').component('hotelRating',{
+    bindings:{
+        stars:'='
+    },
+    template:'<span ng-repeat="star in vm.stars track by $index" class="glyphicon glyphicon-star">{{star}}</span>',
+    controller:'HotelController',
+    controllerAs:'vm'
+});

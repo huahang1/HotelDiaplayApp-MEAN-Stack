653269bae20a4bfa22778bd203417e8eeb4b8bee
Chris Hua <hanghua@HANGs-MacBook-Air.local>
2017-04-07 20:25:01 -0500

    add the user register and login part for the front-end

---
+++ public/angular-app/navigation-directive/navigation-directive.js
@@ -0,0 +1,9 @@
+angular.module('meanhotel').directive('mhNavigation',mhNavigation);
+
+function mhNavigation() {
+
+    return {
+        restrict:'E',
+        templateUrl:'angular-app/navigation-directive/navigation-directive.html'
+    };
+}
